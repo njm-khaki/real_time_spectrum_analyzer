e053c9fe7e8507687e8701d70e83d54544cf3ab3
njm-khaki <39180345+s1523119@users.noreply.github.com>
2021-08-24 00:06:14 +0900

    グラフ部分実装(TODO: Provider適用)

--- realtime_spectrum_analyzer/lib/main.dart
+++ realtime_spectrum_analyzer/lib/main.dart
@@ -1,28 +1,20 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'realtime_spectrum_analyzer',
+      ),
     );
   }
 }
@@ -46,68 +38,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final int _length = 4096;
+  List<double> _data = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _generateRandData();
+  }
+
+  void _generateRandData() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _data = List<double>.filled(_length, 0.0)
+          .map((double _value) => math.Random().nextDouble())
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        alignment: Alignment.center,
+        child: CustomPaint(
+          size: Size(
+            MediaQuery.of(context).size.width,
+            MediaQuery.of(context).size.width / 2
+          ),
+          painter: SpectrumPainter(
+            data: _data,
+            color: Theme.of(context).primaryColor.withOpacity(0.2),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _generateRandData,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class SpectrumPainter extends CustomPainter {
+  final List<double> data;
+  final Color color;
+
+  SpectrumPainter({
+    required List<double> this.data,
+    required Color this.color
+  }) : super();
+
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+
+  @override
+  void paint(Canvas _canvas, Size _size) {
+    final Paint _red = Paint()
+      ..color = color;
+
+    final Rect _plotFrame = Rect.fromLTWH(
+        0,
+        0,
+        _size.width,
+        _size.height,
+    );
+    _canvas.drawRect(_plotFrame, _red);
+
+    for (int i = 0; i < data.length; i++) {
+      if (i == 0) {
+        continue;
+      }
+
+      final Offset _from = _dataMapping(
+        size: _size,
+        x: (i - 1) / data.length,
+        y: data[i - 1],
+      );
+
+      final Offset _to = _dataMapping(
+        size: _size,
+        x: i / data.length,
+        y: data[i],
+      );
+
+      _paintLine(
+          canvas: _canvas,
+          color: Colors.black,
+          from: _from,
+          to: _to,
+      );
+    }
+  }
+
+  void _paintLine({
+    required Canvas canvas,
+    required Color color,
+    required Offset to,
+    Offset from = Offset.zero,
+  }) => canvas.drawLine(
+        from,
+        to,
+        Paint()..color = color,
+    );
+
+  /// 表示領域に対する比率から座標を求める
+  Offset _dataMapping({
+    required Size size,
+    required double x,
+    required double y,
+  }) => Offset(
+    size.width * x,
+    size.height * y
+  );
+
 }
